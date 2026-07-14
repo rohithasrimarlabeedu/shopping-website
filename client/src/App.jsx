04574562c7b313e1ae1585fd036867ddaf9244ff
Rohithasri Marlabeedudu <rohithasrimarlabeedu@gmail.com>
2026-07-14 10:51:44 +0530

    Fix checkout null product error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Cart from "./pages/Cart";
